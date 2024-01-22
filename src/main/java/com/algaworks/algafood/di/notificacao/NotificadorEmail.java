732c22e2ca3184e85d2468b5eebf819d7bcdb78e
Lucas Costa <lnatanfrancacosta@gmail.com>
2024-01-22 15:36:03 -0300

    configuração usando properties e anotação value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,17 @@ import org.springframework.stereotype.Component;
 @TipoDeNotificacao(NivelUrgencia.NORMAL)
 @Profile("prod")
 public class NotificadorEmail implements Notificador {
+
+    @Value("${notificador.email.host}")
+    private String host;
+
+    @Value("${notificador.email.port}")
+    private String port;
+
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("host = " + this.host);
+        System.out.println("port = " + this.port);
         System.out.printf("Cliente %s está sendo notificado pelo email %s: %s%n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
